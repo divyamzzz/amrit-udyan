a8fb19e6a93ed47ae92e60ce4bf130f50d66af8e
divyamzzz <101924382+divyamzzz@users.noreply.github.com>
2023-06-22 02:22:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+for(var i=0;i<6;i++)
+{
+document.querySelectorAll("button")[i].addEventListener("click",function(){
+   
+    document.querySelector("button").classList.toggle("margin");
+    
+})
+}
